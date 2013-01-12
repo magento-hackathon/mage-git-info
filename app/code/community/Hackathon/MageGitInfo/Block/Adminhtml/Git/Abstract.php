dd7c1878b5d6412b778e1e3ae0d50576a909c28e
Ahe <info@andre-herrn.de>
2013-01-12 17:56:00 +0100

    Formatted Class documentation

--- app/code/community/Hackathon/MageGitInfo/Block/Adminhtml/Git/Abstract.php
+++ app/code/community/Hackathon/MageGitInfo/Block/Adminhtml/Git/Abstract.php
@@ -17,6 +17,7 @@
  * @author    Tom Kadwill <tomkadwill@gmail.com>
  * @author    Stephan Hoyer <ste.hoyer@gmail.com>
  * @author    André Herrn <info@andre-herrn.de>
+ * @author    Anjey Lobas <anjey.lobas@goodahead.com>
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @version   $Id:$
  * @since     0.1.0
@@ -29,6 +30,7 @@
  * @author    Tom Kadwill <tomkadwill@gmail.com>
  * @author    Stephan Hoyer <ste.hoyer@gmail.com>
  * @author    André Herrn <info@andre-herrn.de>
+ * @author    Anjey Lobas <anjey.lobas@goodahead.com>
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @version   $Id:$
  * @since     0.1.0
