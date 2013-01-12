b3d0dcacb5a24e90f5e3c445411176acfcbb77fd
Ahe <info@andre-herrn.de#>
2013-01-12 16:15:12 +0100

    Switched layout wrapper rendering from before_body_end to footer by obserer

---
+++ app/code/community/Hackathon/MageGitInfo/Model/Observer.php
@@ -0,0 +1,59 @@
+<?php
+    /**
+     * This file is part of the MAGEGITINFO project.
+     *
+     * Hackathon MageGitInfo is free software; you can redistribute it and/or
+     * modify it under the terms of the GNU General Public License version 3 as
+     * published by the Free Software Foundation.
+     *
+     * This script is distributed in the hope that it will be useful, but WITHOUT
+     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+     * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     *
+     * PHP version 5
+     *
+     * @category  MageGitInfo
+     * @package   Hackathon_MageGitInfo
+     * @author    Tom Kadwill <tomkadwill@gmail.com>
+     * @author    Stephan Hoyer <ste.hoyer@gmail.com>
+     * @author    André Herrn <info@andre-herrn.de>
+     * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+     * @version   $Id:$
+     * @since     0.1.0
+     */
+    /**
+     * Dummy data helper for translation issues.
+     *
+     * @category  MageGitInfo
+     * @package   Hackathon_MageGitInfo
+     * @author    Tom Kadwill <tomkadwill@gmail.com>
+     * @author    Stephan Hoyer <ste.hoyer@gmail.com>
+     * @author    André Herrn <info@andre-herrn.de>
+     * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+     * @version   $Id:$
+     * @since     0.1.0
+     */
+class Hackathon_MageGitInfo_Model_Observer
+{
+    /**
+     * Append parcel announcement to html output.
+     *
+     * @param  $observer
+     * @return void
+     */
+    public function appendGitInfoToFooter($observer)
+    {
+        if ($observer->getBlock() instanceof Mage_Adminhtml_Block_Page_Footer) {
+            $transport = $observer->getTransport();
+            $block     = $observer->getBlock();
+            $layout    = $block->getLayout();
+            $html      = $transport->getHtml();
+
+            $mageGitInfoFooterHtml = $layout->getBlock("magegitinfo_wrapper")->renderView();
+            $html = $html . $mageGitInfoFooterHtml;
+
+            $transport->setHtml($html);
+        }
+    }
+
+}
