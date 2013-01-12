129a51e83bfdd5e24260c12ff7c61c86579a68b7
Ahe <info@andre-herrn.de>
2013-01-12 19:58:29 +0100

    Added Checkout - Switch Branch option and added a command logging

--- app/code/community/Hackathon/MageGitInfo/Helper/Data.php
+++ app/code/community/Hackathon/MageGitInfo/Helper/Data.php
@@ -37,5 +37,18 @@
  */
 class Hackathon_MageGitInfo_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const LOG_FILE_NAME = "magegitinfo.log";
 
+    /**
+     * Log message for var/log/...
+     *
+     * @param $message
+     * @return void
+     */
+    public function log($message)
+    {
+        if (true === Mage::getModel("magegitinfo/config")->getLoggingIsEnabled()) {
+            Mage::log($message, null, self::LOG_FILE_NAME, true);
+        }
+    }
 }
