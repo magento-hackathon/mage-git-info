3d6b60132db23fd7e21e516c802cd8b1d2fb7b46
Tom Kadwill <tom@webshopapps.com>
2013-01-12 19:04:04 +0000

    Added span tag to log output in phtml

--- app/code/community/Hackathon/MageGitInfo/Model/Log.php
+++ app/code/community/Hackathon/MageGitInfo/Model/Log.php
@@ -1,52 +1,61 @@
 <?php
-    /**
-     * This file is part of the MAGEGITINFO project.
-     *
-     * Hackathon MageGitInfo is free software; you can redistribute it and/or
-     * modify it under the terms of the GNU General Public License version 3 as
-     * published by the Free Software Foundation.
-     *
-     * This script is distributed in the hope that it will be useful, but WITHOUT
-     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-     * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-     *
-     * PHP version 5
-     *
-     * @category  MageGitInfo
-     * @package   Hackathon_MageGitInfo
-     * @author    Tom Kadwill <tomkadwill@gmail.com>
-     * @author    Stephan Hoyer <ste.hoyer@gmail.com>
-     * @author    André Herrn <info@andre-herrn.de>
-     * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-     * @version   $Id:$
-     * @since     0.1.0
-     */
-    /**
-     * Dummy data helper for translation issues.
-     *
-     * @category  MageGitInfo
-     * @package   Hackathon_MageGitInfo
-     * @author    Tom Kadwill <tomkadwill@gmail.com>
-     * @author    Stephan Hoyer <ste.hoyer@gmail.com>
-     * @author    André Herrn <info@andre-herrn.de>
-     * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-     * @version   $Id:$
-     * @since     0.1.0
-     */
-    class Hackathon_MageGitInfo_Model_Log extends Hackathon_MageGitInfo_Model_Git
-    {
-        public $output = array();
+/**
+ * This file is part of the MAGEGITINFO project.
+ *
+ * Hackathon MageGitInfo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  MageGitInfo
+ * @package   Hackathon_MageGitInfo
+ * @author    Tom Kadwill <tomkadwill@gmail.com>
+ * @author    Stephan Hoyer <ste.hoyer@gmail.com>
+ * @author    André Herrn <info@andre-herrn.de>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+/**
+ * Dummy data helper for translation issues.
+ *
+ * @category  MageGitInfo
+ * @package   Hackathon_MageGitInfo
+ * @author    Tom Kadwill <tomkadwill@gmail.com>
+ * @author    Stephan Hoyer <ste.hoyer@gmail.com>
+ * @author    André Herrn <info@andre-herrn.de>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+class Hackathon_MageGitInfo_Model_Log extends Hackathon_MageGitInfo_Model_Git
+{
+    public $output = array();
 
-        public function gitLog($numberOfLogs='1') {
-            $statement = 'git log -v -n' . $numberOfLogs;
-            $this->exec($statement);
-            $output = $this->output;
-            foreach ($output as $line){
-                if ($line != '' && is_numeric(strpos($line, 'commit')) || is_numeric(strpos($line, 'Author'))) {
+    public function gitLog($numberOfLogs='1') {
+        $statement = 'git log -v -n' . (string)$numberOfLogs;
+        $this->exec($statement);
+        $output = $this->output;
+        $formattedLogsString = '';
+        foreach ($output as $line){
+            if ($line != '' && is_numeric(strpos($line, 'commit')) || is_numeric(strpos($line, 'Author'))) {
+                if (is_numeric(strpos($line, 'commit')) & strpos($line, 'commit') == 0) {
                     $line = str_replace('commit', '',$line);
-                    $formattedLogs[] = $line;
+                    $class = 'commit';
+                } elseif (is_numeric(strpos($line, 'Author'))) {
+                    $class = 'author';
+                } else {
+                    $class = 'comment';
                 }
+                $formattedLogs[] = $line;
+                $formattedLogsString = $formattedLogsString . '<span class="'. $class . '">' . $line . '</span></br>';
             }
-            return $formattedLogs;
         }
+        return $formattedLogsString;
     }
+}
